bc669b1d78838a7ed2ca51cdef1d5a3981da8692
Vinicius <m.vini846@gmail.com>
2016-02-04 11:26:59 -0300

    integrando com o backend(node)

--- js/script.js
+++ js/script.js
@@ -1,30 +1,40 @@
 /*Criando um modulo*/ 
 		angular.module("listaTele", []);
 		/* Lendo um modulo */
-		angular.module("listaTele").controller("listaCtrl", function ($scope, $filter){
+		angular.module("listaTele").controller("listaCtrl", function ($scope, $filter, $http){
 			$scope.app = "Lista Telefônica";
-			$scope.contatos = [
-				{nome: $filter('uppercase')("Pedro"), telefone: "8888-8888" , data: new Date(), operadora: {nome:"Oi", codigo: "12"}, cor: "red"},
-				{nome: "Ana", telefone: "8888-8887" ,  data: new Date(), operadora: {nome:"Tim", codigo: "18"}, cor: "blue"},
-				{nome: "Maria", telefone: "8888-8886" , data: new Date(), operadora: {nome:"Vivo", codigo: "15"}, cor: "green"}
-			];
-			$scope.operadoras = [
-				{nome: "Oi", codigo: "12", preco: 3},
-				{nome: "Vivo", codigo: "15", preco: 1},
-				{nome: "Tim", codigo: "18", preco: 4},
-				{nome: "Claro", codigo: "19", preco: 2}
-			];
+			$scope.contatos = [];
+			$scope.operadoras = [];
+
+			var carregarOperadoras = function(){
+				$http.get("http://localhost:3000/operadoras").success(function(data){
+					$scope.operadoras = data;
+				});
+			};
+
+			var carregarContatos = function(){
+				$http.get("http://localhost:3000/contatos").success(function(data){
+					$scope.contatos = data;
+				}).error(function(data,status){
+					$scope.message = "Aconteceu um erro! " + data;
+				});
+			};
 
 			$scope.apagarContatos = function (contatos){
 				$scope.contatos=contatos.filter(function (cont){
 					if (!cont.selecionado){
 						return cont;
+						carregarContatos();
 					}
 				});
 			};
 
 			$scope.adicionarContatos = function (contato){
-				$scope.contatos.push(angular.copy(contato));
+				contato.data = new Date();
+				$http.post("http://localhost:3000/contatos", contato).success( function (data){
+					delete $scope.contato;
+					carregarContatos();
+				});
 			};
 
 			$scope.mandar = function(contatos){
@@ -38,5 +48,8 @@
 				$scope.criterioOrdenacao = campo;
 				$scope.direcaoOrdenacao = !$scope.direcaoOrdenacao;
 			}
+
+			carregarContatos();
+			carregarOperadoras();
 
 		});	
--- json.js
+++ json.js
@@ -1,7 +1,52 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function(req,res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain'});
-	res.end("{'nome':'Ythalo','telefone':'1111-2222','data':'02/12/2006','operadora':{'nome':'Oi','codigo':'12','categoria':'celular'}},{'nome':'José','telefone':'5555-3333','data':'15/09/2010','operadora':{'nome':'Vivo','codigo':'15','categoria':'celular'}},{'nome':'Edgar','telefone':'6666-7777','data':'21/01/2013','operadora':{'nome':'Tim','codigo':'18','categoria':'celular'}}");
-}).listen(3000);
-console.log('Servidor iniciado em localhost:3000. Ctrl+C para encerrar…');
+app.use(express.static(__dirname + '/public'));
+app.use(express.bodyParser());
+
+var contatos = [
+	{nome: "Bruno", telefone: "9999-2222", data: new Date(), operadora: {nome: "Oi", codigo: 14, categoria: "Celular"}},
+	{nome: "Sandra", telefone: "9999-3333", data: new Date(), operadora: {nome: "Vivo", codigo: 15, categoria: "Celular"}},
+	{nome: "Mariana", telefone: "9999-9999", data: new Date(), operadora: {nome: "Tim", codigo: 41, categoria: "Celular"}}
+];
+var operadoras = [
+	{nome: "Oi", codigo: 14, categoria: "Celular", preco: 2},
+	{nome: "Vivo", codigo: 15, categoria: "Celular", preco: 1},
+	{nome: "Tim", codigo: 41, categoria: "Celular", preco: 3},
+	{nome: "GVT", codigo: 25, categoria: "Fixo", preco: 1},
+	{nome: "Embratel", codigo: 21, categoria: "Fixo", preco: 2}
+];
+
+app.listen(process.env.PORT || 3000);
+
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.get('/contatos', function(req, res) {
+  res.json(contatos);
+});
+
+app.post('/contatos', function(req, res) {
+  contatos.push(req.body);
+  res.json(true);
+});
+
+app.get('/operadoras', function(req, res) {
+  res.json(operadoras);
+});
+
+
+
+// Inicializa a função no console
+	/*var get = function(url, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState === 4){
+			callback(xhr.responseText, xhr.status);
+		}
+	};		
+};*/
